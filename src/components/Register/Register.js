630e5abad7a0130952b1388827649a7ecb378147
robmartel <99683835+robmartel@users.noreply.github.com>
2023-04-28 13:05:46 -0400

    changed register back

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   };
 
   onSubmitSignIn = () => {
-    fetch('https://smartbrainbackend-oecs.onrender.com', {
+    fetch('https://smartbrainbackend-oecs.onrender.com/register', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
